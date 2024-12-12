26e2900c1f172e126055f99177adf7bc524e2a9c
Todd Pihl <todd.pihl@gmail.com>
2024-12-12 17:02:32 -0500

    Added CDE mapping

--- ModelMappingFileGenerator.py
+++ ModelMappingFileGenerator.py
@@ -13,52 +13,82 @@ def getCDEInfo(term_object):
             return {'cdeid':term_dict['origin_id'], 'cdeversion': term_dict['origin_version']}
         else:
             return{'cdeid':None, 'cdeversion': None}
+        
+def makeMDFDataFrame(mdf):
+    #Create a dataframe from the model file
+    columns = ["node", "property", "cdeid", "cdeversion"]
+    mdf_df = pd.DataFrame(columns=columns)
+    nodes = mdf.model.nodes
+    nodelist = list(nodes.keys())
+    for node in nodelist:
+        propertylist = list(nodes[node].props.keys())
+        for property in propertylist:
+            if mdf.model.props[node, property].concept is not None:
+                concept = mdf.model.props[node, property].concept.terms
+                if concept is not None:
+                    cdeinfo = getCDEInfo(concept)
+                    mdf_df.loc[len(mdf_df)] = {"node": node, "property": property, "cdeid": cdeinfo['cdeid'], "cdeversion":cdeinfo["cdeversion"]}
+    return mdf_df
+            
+
+def cdeCheck2(oldid, oldver, newid, newver):
+    if oldid == newid:
+        if oldver == newver:
+            return "CDE ID and Version match"
+        else:
+            return "CDE Version mismatch"
+    else:
+        return "CDE ID mismarch"
+
+def cdeMatch(old_df, new_df, liftover_df, old_version, new_version):
+    #Instead of string matching, match by CDE ID.  Ignore CDE version
+    for index, row in old_df.iterrows():
+        # Meant to handle previous mappings like crdc_id
+        if row['property'] not in liftover_df['lift_from_property']:
+            if (new_df['cdeid'].eq(row['cdeid'])).any():
+                temp_df = new_df.loc[new_df['cdeid'] == row['cdeid']]
+                for tempindex, temprow in temp_df.iterrows():
+                    cde_relationship = cdeCheck2(row['cdeid'], row['cdeversion'], temprow['cdeid'], temprow['cdeversion'])
+                    liftover_df.loc[len(liftover_df)]= {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
+                                                         "lift_to_version": new_version, "lift_to_node": temprow['node'], "lift_to_property": temprow['property'], "lift_from_cde":row['cdeid'], 
+                                                         "lift_from_cdeversion": row['cdeversion'], "lift_to_cde": temprow['cdeid'], "lift_to_cdeversion": temprow['cdeversion'], "cde_relationship": cde_relationship}
+    return liftover_df
+            
+    
+def stringMatch(old_df, new_df, liftover_df, old_version, new_version):
+    for index, row in old_df.iterrows():
+        #Check that the property hasn't already been mapped by CDE
+        if row['property'] not in liftover_df['lift_from_property']:
+            # If no, then see if there is a string match
+            if row['property'] in new_df['property']:
+                temp_df = new_df.loc[new_df['property'] == row['property']]
+                for tempindex, temprow in temp_df.iterrows():
+                    cde_relationship = cdeCheck2(row['cdeid'], row['cdeversion'], temprow['cdeid'], temprow['cdeversion'])
+                    liftover_df.loc[len(liftover_df)]= {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
+                                                         "lift_to_version": new_version, "lift_to_node": temprow['node'], "lift_to_property": temprow['property'], "lift_from_cde":row['cdeid'], 
+                                                         "lift_from_cdeversion": row['cdeversion'], "lift_to_cde": temprow['cdeid'], "lift_to_cdeversion": temprow['cdeverion'], "cde_relationship": cde_relationship}
+            # If no string match
+            else:
+                cde_relationship = "Unmapped"
+                liftover_df.loc[len(liftover_df)]= {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
+                                                         "lift_to_version": new_version, "lift_to_node": "N/A", "lift_to_property": "N/A", "lift_from_cde":row['cdeid'], 
+                                                         "lift_from_cdeversion": row['cdeversion'], "lift_to_cde": "N/A", "lift_to_cdeversion": "N/a", "cde_relationship": cde_relationship}
+                
+        return liftover_df
     
 
-def cdeCheck(old_node, old_prop, new_node, new_prop, old_mdf, new_mdf):
-    #This tries to provide information from CDEs, not just text matching
-    old_concept = None
-    new_concept = None
-    relation = None
-    if old_mdf.model.props[(old_node, old_prop)].concept is not None:
-        old_concept = old_mdf.model.props[(old_node, old_prop)].concept.terms
-    if new_mdf.model.props[new_node, new_prop].concept is not None:
-        new_concept = new_mdf.model.props[(new_node, new_prop)].concept.terms
-    if old_concept is not None:
-        old_cde = getCDEInfo(old_concept)
-    else:
-        old_cde = {'cdeid':None, 'cdeversion': None}
-    if new_concept is not None:
-        new_cde = getCDEInfo(new_concept)
-    else:
-        new_cde = {'cdeid':None, 'cdeversion': None}
-    #Need to check for blanks in cde id
-    if(old_cde['cdeid'] == None) or (old_cde['cdeversion'] == None):
-        relation = "Missing CDE"
-    elif (old_cde['cdeid'] == new_cde['cdeid']) and (old_cde['cdeversion'] == new_cde['cdeversion']):
-        relation = "Match CDE and Version"
-    elif (old_cde['cdeid'] == new_cde['cdeid']) and (old_cde['cdeversion'] != new_cde['cdeversion']):
-        relation = "Version mismatch"
-    elif (old_cde['cdeid'] != new_cde['cdeid']):
-        relation = "CDE mismatch"
-    else:
-        relation = "Indeterminant"
-    return {'old_cde': old_cde['cdeid'], 'old_version': old_cde['cdeversion'], 'new_cde': new_cde['cdeid'], 'new_version': new_cde['cdeversion'], 'relation':relation}
-        
-        
-
-def idAdd(old_node, old_property, liftover_df, new_props, old_version, new_version):
-    #Handle the crdc_id issue
-    if (old_node, old_property) in new_props:
-        #Since the check was explicitly if (old_node, old_property) exists in new_props, anything passing means new==old for both and the statements below are NOT a typo
-        liftover_df.loc[len(liftover_df)] = {"lift_from_version": old_version, "lift_from_node": old_node, "lift_from_property": old_property, 
-                                                         "lift_to_version": new_version, "lift_to_node": old_node, "lift_to_property": old_property, "lift_from_cde":"N/A", 
+def crdcIDAdd(old_df, liftover_df, new_props, old_version, new_version):
+    old_crdc_df = old_df.loc[old_df['property'] == 'crdc_id']
+    for index, row in old_crdc_df.iterrows():
+        if (row['node'], row['property']) in new_props:
+            liftover_df.loc[len(liftover_df)] = {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
+                                                         "lift_to_version": new_version, "lift_to_node": row['node'], "lift_to_property": row['property'], "lift_from_cde":"N/A", 
                                                          "lift_from_cdeversion": "N/A", "lift_to_cde": "N/A", "lift_to_cdeversion": "N/A", "cde_relationship": "N/A"}
-        
-    else:
-        liftover_df.loc[len(liftover_df)] = {"lift_from_version": old_version, "lift_from_node": old_node, "lift_from_property": old_property, 
+        else:
+            liftover_df.loc[len(liftover_df)] = {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
                                                          "lift_to_version": new_version, "lift_to_node": "N/A", "lift_to_property": "N/A", "lift_from_cde":"N/A", 
                                                          "lift_from_cdeversion": "N/A", "lift_to_cde": "N/A", "lift_to_cdeversion": "N/A", "cde_relationship": "N/A"}
+        
     return liftover_df
 
 
@@ -74,39 +104,26 @@ def main(args):
     old_mdf = bento_mdf.MDF(*configs["old_version_files"])
     new_mdf = bento_mdf.MDF(*configs["new_version_files"])
     
-    old_model_version = old_mdf.version
-    print(f"Old Model version:\t{old_model_version}")
-    new_model_version = new_mdf.version
-    print(f"New Model Version:\t{new_model_version}")
-    
-    old_nodes = old_mdf.model.nodes
-    old_nodes_list = list(old_nodes.keys())
     new_props = new_mdf.model.props
     
-    for old_node in old_nodes_list:
-        #Get the properties associated with the node
-        old_node_properties = list(old_nodes[old_node].props.keys())
-        for old_property in old_node_properties:
-            notfoundprop = True
-            if old_property == 'crdc_id':
-                liftover_df = idAdd(old_node, old_property, liftover_df, new_props, old_model_version, new_model_version)
-                notfoundprop = False
-            else:
-                for x in new_props:
-                    if old_property in x:
-                        new_node = x[0]
-                        new_property = x[1]
-                        cdeinfo = cdeCheck(old_node, old_property, new_node, new_property, old_mdf, new_mdf)
-                        liftover_df.loc[len(liftover_df)] = {"lift_from_version": old_model_version, "lift_from_node": old_node, "lift_from_property": old_property, 
-                                                         "lift_to_version": new_model_version, "lift_to_node": new_node, "lift_to_property": new_property,
-                                                         "lift_from_cde":cdeinfo['old_cde'], "lift_from_cdeversion": cdeinfo['old_version'], "lift_to_cde": cdeinfo['new_cde'],
-                                                         "lift_to_cdeversion": cdeinfo['new_version'], "cde_relationship": cdeinfo['relation']}
-                        notfoundprop = False
-        if notfoundprop:
-            liftover_df.loc[len(liftover_df)] = {"lift_from_version": old_model_version, "lift_from_node": old_node, "lift_from_property": old_property, 
-                                                         "lift_to_version": new_model_version, "lift_to_node": "Orphan", "lift_to_property":"Orphan", 
-                                                         "lift_from_cde": "N/A", "lift_from_cdeversion": "N/A", "lift_to_cde": "N/A",
-                                                         "lift_to_cdeversion": "N/A", "cde_relationship": "N/A"}
+    old_model_version = old_mdf.version
+    new_model_version = new_mdf.version
+    
+    # Create the dataframes
+    old_df = makeMDFDataFrame(old_mdf)
+    new_df = makeMDFDataFrame(new_mdf)
+    
+    
+    #Deal with crdc_id
+    liftover_df = crdcIDAdd(old_df, liftover_df, new_props, old_model_version, new_model_version)
+    
+    #Match by CDE first
+    print("Map by CDE")
+    liftover_df = cdeMatch(old_df, new_df, liftover_df, old_model_version, new_model_version)
+    
+    #Do exact string match for any fields that didn't match by CDE
+    liftover_df = stringMatch(old_df, new_df, liftover_df, old_model_version, new_model_version)
+    
             
     # Finally, add the realtionship columns to the mapping file.  These aren't model dependent.  I think.
     relations = configs['relationship_columns']
