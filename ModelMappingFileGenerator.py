59ef8b6a42c512f2df36adcbafc096046821fa5e
Todd Pihl <todd.pihl@gmail.com>
2024-12-20 15:47:37 -0500

    Work on concept mapping

--- ModelMappingFileGenerator.py
+++ ModelMappingFileGenerator.py
@@ -41,7 +41,7 @@ def cdeCheck2(oldid, oldver, newid, newver):
     else:
         return "CDE ID mismarch"
 
-def cdeMatch(old_df, new_df, liftover_df, old_version, new_version):
+def cdeMatch(old_df, new_df, liftover_df, old_version, new_version, old_handle, new_handle):
     #Instead of string matching, match by CDE ID.  Ignore CDE version
     for index, row in old_df.iterrows():
         # Meant to handle previous mappings like crdc_id
@@ -50,13 +50,14 @@ def cdeMatch(old_df, new_df, liftover_df, old_version, new_version):
                 temp_df = new_df.loc[new_df['cdeid'] == row['cdeid']]
                 for tempindex, temprow in temp_df.iterrows():
                     cde_relationship = cdeCheck2(row['cdeid'], row['cdeversion'], temprow['cdeid'], temprow['cdeversion'])
-                    liftover_df.loc[len(liftover_df)]= {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
-                                                         "lift_to_version": new_version, "lift_to_node": temprow['node'], "lift_to_property": temprow['property'], "lift_from_cde":row['cdeid'], 
-                                                         "lift_from_cdeversion": row['cdeversion'], "lift_to_cde": temprow['cdeid'], "lift_to_cdeversion": temprow['cdeversion'], "cde_relationship": cde_relationship}
+                    liftover_df.loc[len(liftover_df)]= {"lift_from_model": old_handle, "lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
+                                                         "lift_to_model": new_handle, "lift_to_version": new_version, "lift_to_node": temprow['node'], "lift_to_property": temprow['property'],
+                                                         "lift_from_cde":row['cdeid'], "lift_from_cdeversion": row['cdeversion'], "lift_to_cde": temprow['cdeid'], 
+                                                         "lift_to_cdeversion": temprow['cdeversion'], "cde_relationship": cde_relationship}
     return liftover_df
             
     
-def stringMatch(old_df, new_df, liftover_df, old_version, new_version):
+def stringMatch(old_df, new_df, liftover_df, old_version, new_version, old_handle, new_handle):
     for index, row in old_df.iterrows():
         #Check that the property hasn't already been mapped by CDE
         if row['property'] not in liftover_df['lift_from_property']:
@@ -65,29 +66,30 @@ def stringMatch(old_df, new_df, liftover_df, old_version, new_version):
                 temp_df = new_df.loc[new_df['property'] == row['property']]
                 for tempindex, temprow in temp_df.iterrows():
                     cde_relationship = cdeCheck2(row['cdeid'], row['cdeversion'], temprow['cdeid'], temprow['cdeversion'])
-                    liftover_df.loc[len(liftover_df)]= {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
-                                                         "lift_to_version": new_version, "lift_to_node": temprow['node'], "lift_to_property": temprow['property'], "lift_from_cde":row['cdeid'], 
-                                                         "lift_from_cdeversion": row['cdeversion'], "lift_to_cde": temprow['cdeid'], "lift_to_cdeversion": temprow['cdeverion'], "cde_relationship": cde_relationship}
+                    liftover_df.loc[len(liftover_df)]= {"lift_from_model": old_handle, "lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
+                                                         "lift_to_model": new_handle, "lift_to_version": new_version, "lift_to_node": temprow['node'], "lift_to_property": temprow['property'],
+                                                         "lift_from_cde":row['cdeid'], "lift_from_cdeversion": row['cdeversion'], "lift_to_cde": temprow['cdeid'], 
+                                                         "lift_to_cdeversion": temprow['cdeverion'], "cde_relationship": cde_relationship}
             # If no string match
             else:
                 cde_relationship = "Unmapped"
-                liftover_df.loc[len(liftover_df)]= {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
-                                                         "lift_to_version": new_version, "lift_to_node": "N/A", "lift_to_property": "N/A", "lift_from_cde":row['cdeid'], 
+                liftover_df.loc[len(liftover_df)]= {"lift_from_model": old_handle, "lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
+                                                         "lift_to_model": new_handle, "lift_to_version": new_version, "lift_to_node": "N/A", "lift_to_property": "N/A", "lift_from_cde":row['cdeid'], 
                                                          "lift_from_cdeversion": row['cdeversion'], "lift_to_cde": "N/A", "lift_to_cdeversion": "N/a", "cde_relationship": cde_relationship}
                 
         return liftover_df
     
 
-def crdcIDAdd(old_df, liftover_df, new_props, old_version, new_version):
+def crdcIDAdd(old_df, liftover_df, new_props, old_version, new_version, old_handle, new_handle):
     old_crdc_df = old_df.loc[old_df['property'] == 'crdc_id']
     for index, row in old_crdc_df.iterrows():
         if (row['node'], row['property']) in new_props:
-            liftover_df.loc[len(liftover_df)] = {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
-                                                         "lift_to_version": new_version, "lift_to_node": row['node'], "lift_to_property": row['property'], "lift_from_cde":"N/A", 
+            liftover_df.loc[len(liftover_df)] = {"lift_from_model": old_handle, "lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
+                                                         "lift_to_model": new_handle, "lift_to_version": new_version, "lift_to_node": row['node'], "lift_to_property": row['property'], "lift_from_cde":"N/A", 
                                                          "lift_from_cdeversion": "N/A", "lift_to_cde": "N/A", "lift_to_cdeversion": "N/A", "cde_relationship": "N/A"}
         else:
-            liftover_df.loc[len(liftover_df)] = {"lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
-                                                         "lift_to_version": new_version, "lift_to_node": "N/A", "lift_to_property": "N/A", "lift_from_cde":"N/A", 
+            liftover_df.loc[len(liftover_df)] = {"lift_from_model": old_handle, "lift_from_version": old_version, "lift_from_node": row['node'], "lift_from_property": row['property'], 
+                                                         "lift_to_model": new_handle, "lift_to_version": new_version, "lift_to_node": "N/A", "lift_to_property": "N/A", "lift_from_cde":"N/A", 
                                                          "lift_from_cdeversion": "N/A", "lift_to_cde": "N/A", "lift_to_cdeversion": "N/A", "cde_relationship": "N/A"}
         
     return liftover_df
@@ -123,13 +125,15 @@ def getcdeJSON(cdeid, cdeversion):
 
 
 def createValueDF(mdf_df, verbose=False):
-    columns = ["cdeID", "cdeVersion","permissibleValue", "valueTerm", "ncitCode"]
+    columns = ["node", "property", "cdeID", "cdeVersion","permissibleValue", "valueTerm", "ncitCode"]
     value_df = pd.DataFrame(columns=columns)
     #lf = open(r".\examples\troubleshooting.txt", "a", encoding="utf-8")
 
     for index, row in mdf_df.iterrows():
         #print(row)
         if row['cdeid'] is not None:
+            node = row['node']
+            prop = row['property']
             cdeid = row['cdeid']
             cdeversion = row['cdeversion']
             #print(f"Starting CDE: {cdeid}\tStarting Version:{cdeversion}")
@@ -156,7 +160,8 @@ def createValueDF(mdf_df, verbose=False):
                         if len(permissiblevalue['ValueMeaning']['Concepts']) > 0:
                             #print("there are concepts")
                             for concept in permissiblevalue['ValueMeaning']['Concepts']:
-                                value_df.loc[len(value_df)] = {"cdeID": cdeid, "cdeVersion": cdeversion, "permissibleValue": pv, "valueTerm": concept['longName'], "ncitCode": concept['conceptCode']}
+                                value_df.loc[len(value_df)] = {"node": node, "property": prop, "cdeID": cdeid, "cdeVersion": cdeversion, 
+                                                               "permissibleValue": pv, "valueTerm": concept['longName'], "ncitCode": concept['conceptCode']}
                                 #print(str({"cdeID": cdeid, "cdeVersion": cdeversion, "valueTerm": concept['longName'], "ncitCode": concept['conceptCode']}))
     # Remove duplicate lines
     value_df.drop_duplicates(keep='first', inplace=True)
@@ -168,26 +173,46 @@ def conceptMatch(old_value_df, new_value_df, value_liftover_df, old_model_versio
     # TODO: rethink output, row[valueTerm] is wrong.  And probably need a node reference
     # Probably need node:property:pv
     for index, row, in old_value_df.iterrows():
+        # First, look for an NCIt code match
         if new_value_df['ncitCode'].eq(row['ncitCode']).any():
             temp_df = new_value_df.loc[new_value_df['ncitCode'] == row['ncitCode']]
             for tempindex, temprow in temp_df.iterrows():
                 relationship = "Concept Code Match"
-                value_liftover_df.loc[len(value_liftover_df)] = {"lift_from_model": old_model_handle, "lift_from_version": old_model_version, "lift_from_property": row['valueTerm'], "lift_to_model": new_model_handle, "lift_to_version": new_model_version, 
-                                                                 "lift_to_property": temprow['valueTerm'], "lift_from_concept": row['ncitCode'], 
-                                                                 "lift_to_concept": temprow['ncitCode'], "concept_relationship": relationship}
+                # ["node", "property", "cdeID", "cdeVersion","permissibleValue", "valueTerm", "ncitCode"]
+                # value_columns = ["lift_from_model","lift_from_version", "lift_from_node", "lift_from_property", "lift_from_concept", "lift_from_conceptCode",
+                #         "lift_to_model", "lift_to_version", "lift_to_node", "lift_to_property", "lift_to_concept", "lift_to_concepCode", "concept_relationship"]
+                value_liftover_df.loc[len(value_liftover_df)] = {"lift_from_model": old_model_handle, "lift_from_version": old_model_version, "lift_from_node": row['node'], 
+                                                                 "lift_from_property": row['property'], "lift_from_concept": row['permissibleValue'], "lift_from_conceptCode": row['ncitCode'],
+                                                                 "lift_to_model": new_model_handle, "lift_to_version": new_model_version, "lift_to_node": temprow['node'], 
+                                                                 "lift_to_property": temprow['property'], "lift_to_concept": temprow['permissibleValue'], 
+                                                                 "lift_to_conceptCode": temprow['ncitCode'], "concept_relationship": relationship}
+        # If no code match, look for string match on valueTerm, which is probably more standardized than the PV
+        elif new_value_df['valueTerm'].eq(row['valueTerm']).any():
+            temp_df = new_value_df.loc[new_value_df['valueTerm'] == row['valueTerm']]
+            for tempindex, temprow in temp_df.iterrows():
+                relationship = "Value Term Match"
+                value_liftover_df.loc[len(value_liftover_df)] = {"lift_from_model": old_model_handle, "lift_from_version": old_model_version, "lift_from_node": row['node'], 
+                                                                 "lift_from_property": row['property'], "lift_from_concept": row['permissibleValue'], "lift_from_conceptCode": row['ncitCode'],
+                                                                 "lift_to_model": new_model_handle, "lift_to_version": new_model_version, "lift_to_node": temprow['node'], 
+                                                                 "lift_to_property": temprow['property'], "lift_to_concept": temprow['permissibleValue'], 
+                                                                 "lift_to_conceptCode": temprow['ncitCode'], "concept_relationship": relationship}
+        # and a complete strike out
         else:
             relationship = "No match"
-            value_liftover_df.loc[len(value_liftover_df)] = {"lift_from_version": old_model_version, "lift_from_property": row['valueTerm'], "lift_to_version": new_model_version, 
-                                                                 "lift_to_property": 'N/A', "lift_from_concept": row['ncitCode'],
-                                                                 "lift_to_concept": "N/A", "concept_relationship": relationship}
+            value_liftover_df.loc[len(value_liftover_df)] = {"lift_from_model": old_model_handle, "lift_from_version": old_model_version, "lift_from_node": row['node'], 
+                                                                 "lift_from_property": row['property'], "lift_from_concept": row['permissibleValue'], "lift_from_conceptCode": row['ncitCode'],
+                                                                 "lift_to_model": new_model_handle, "lift_to_version": new_model_version, "lift_to_node": "N/A", 
+                                                                 "lift_to_property": "N/A", "lift_to_concept": "N/A", 
+                                                                 "lift_to_conceptCode": "N/A", "concept_relationship": relationship}
     return value_liftover_df
 
 def main(args):
     configs = crdclib.readYAML(args.configfile)
     
     #Dataframe setup
-    columns = ["lift_from_model", "lift_from_version", "lift_from_node", "lift_from_property", "lift_to_model", "lift_to_version", "lift_to_node", "lift_to_property", "lift_from_cde", "lift_from_cdeversion",
-               "lift_to_cde", "lift_to_cdeversion", "cde_relationship"]
+    columns = ["lift_from_model", "lift_from_version", "lift_from_node", "lift_from_property", 
+               "lift_to_model", "lift_to_version", "lift_to_node", "lift_to_property",
+               "lift_from_cde", "lift_from_cdeversion", "lift_to_cde", "lift_to_cdeversion", "cde_relationship"]
     liftover_df = pd.DataFrame(columns=columns)
     
     old_mdf = bento_mdf.MDF(*configs["old_version_files"])
@@ -213,22 +238,23 @@ def main(args):
         new_value_df = createValueDF(new_df, args.verbose)
         if args.verbose:
             new_value_df.to_csv(r".\examples\new_value_df.csv", sep="\t", index=False)
-        value_columns = ["lift_from_version", "lift_from_property", "lift_to_version", "lift_to_property", "lift_from_concept", "lift_to_concept", "concept_relationship"]
+        value_columns = ["lift_from_model","lift_from_version", "lift_from_node", "lift_from_property", "lift_from_concept", "lift_from_conceptCode",
+                         "lift_to_model", "lift_to_version", "lift_to_node", "lift_to_property", "lift_to_concept", "lift_to_conceptCode", "concept_relationship"]
         value_liftover_df = pd.DataFrame(columns=value_columns)
         
         #Do the NCIT concept code mapping
         value_liftover_df = conceptMatch(old_value_df, new_value_df, value_liftover_df, old_model_version, new_model_version, old_model_handle, new_model_handle)
-        value_liftover_df.drop_duplicates(subset=['lift_from_concept', 'lift_to_concept'], keep='first')
+        value_liftover_df.drop_duplicates(subset=['lift_from_concept', 'lift_to_concept'], keep='first', inplace=True)
     
     
     #Deal with crdc_id
-    liftover_df = crdcIDAdd(old_df, liftover_df, new_props, old_model_version, new_model_version)
+    liftover_df = crdcIDAdd(old_df, liftover_df, new_props, old_model_version, new_model_version, old_model_handle, new_model_handle)
     
     #Match by CDE first
-    liftover_df = cdeMatch(old_df, new_df, liftover_df, old_model_version, new_model_version)
+    liftover_df = cdeMatch(old_df, new_df, liftover_df, old_model_version, new_model_version, old_model_handle, new_model_handle)
     
     #Do exact string match for any fields that didn't match by CDE
-    liftover_df = stringMatch(old_df, new_df, liftover_df, old_model_version, new_model_version)
+    liftover_df = stringMatch(old_df, new_df, liftover_df, old_model_version, new_model_version, old_model_handle, new_model_handle)
     
             
     # Finally, add the realtionship columns to the mapping file.  These aren't model dependent.  I think.
